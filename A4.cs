18797042a0ca25db796e78777187666a2f475262
Mitchell Aninyang <aninyang.mitchell@gmail.com>
2019-11-09 12:44:04 -0500

    Add files via upload

--- A4.cs
+++ A4.cs
@@ -1,40 +1,72 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace COMP10066_Lab4
+namespace oosAssignment4
 {
     /**
      * Library of statistical functions using Generics for different statistical
      * calculations.
      * 
-     
      * see http://www.calculator.net/standard-deviation-calculator.html
      * for sample standard deviation calculations
      *
      * @author Joey Programmer
      */
 
+    /*
+     * Name: MITCHELL ANINYANG, 000796709
+     * 
+     * ==================================
+     * STATEMENT OF AUTHORSHIP
+     * ==================================
+     * 
+     * I certify that all code submitted is my own work; 
+     * that I have not copied it from any other source.  
+     * I also certify that I have not allowed my work to be copied by others.
+     * 
+     */
     public class A4
     {
-        public static double Avg(List<double> x, bool incneg)
+        /// <summary>
+        /// Method name = Avg
+        /// This will calculate the average
+        /// I took out the bool incneg argument from the this method
+        /// 
+        /// i am going to round the result of this method to 2 decimal places
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        public static double Avg(List<double> x)
         {
-            double s = Sum(x, incneg);
-            int c = 0;
+            double sum = Sum(x);    //Previously known as int s = Sum(x);
+            int counter = 0;        //previously known as int c = 0;
             for (int i = 0; i < x.Count; i++)
             {
-                if (incneg || x[i] >= 0)
+                if (x[i] >= 0)
                 {
-                    c++;
+                    counter++;
                 }
             }
-            if (c == 0)
+            if (counter == 0)
             {
                 throw new ArgumentException("no values are > 0");
             }
-            return s / c;
+            //return (sum / counter);
+
+            return Math.Round((sum / counter) , 3);         //rounding the average to 2 decimal places
         }
 
-        public static double Sum(List<double> x, bool incneg)
+        /// <summary>
+        /// This is the sum
+        /// 
+        /// i will round the result of this method to 2 decimal places
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        public static double Sum(List<double> x)
         {
             if (x.Count < 0)
             {
@@ -42,16 +74,23 @@ namespace COMP10066_Lab4
             }
 
             double sum = 0.0;
-            foreach (double val in x)
+            foreach (double value in x)     //previously known as double val in x
             {
-                if (incneg || val >= 0)
+                if (value >= 0)
                 {
-                    sum += val;
+                    sum += value;
                 }
             }
-            return sum;
+            return Math.Round(sum, 3);
         }
 
+        /// <summary>
+        /// This wilil calculate the median
+        /// 
+        /// I will round the result of this calculation to 2 decimal places
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
         public static double Median(List<double> data)
         {
             if (data.Count == 0)
@@ -65,9 +104,14 @@ namespace COMP10066_Lab4
             if (data.Count % 2 == 0)
                 median = (data[data.Count / 2] + data[data.Count / 2 - 1]) / 2;
 
-            return median;
+            return Math.Round(median, 3);
         }
 
+        /// <summary>
+        /// This will calculate the standard deviation
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
         public static double SD(List<double> data)
         {
             if (data.Count <= 1)
@@ -77,7 +121,7 @@ namespace COMP10066_Lab4
 
             int n = data.Count;
             double s = 0;
-            double a = Avg(data, true);
+            double a = Avg(data);
 
             for (int i = 0; i < n; i++)
             {
@@ -85,21 +129,31 @@ namespace COMP10066_Lab4
                 s += Math.Pow(v - a, 2);
             }
             double stdev = Math.Sqrt(s / (n - 1));
-            return stdev;
+            return Math.Round(stdev, 3);
         }
 
         // Simple set of tests that confirm that functions operate correctly
         static void Main(string[] args)
         {
+            //List of numbers
             List<Double> testDataD = new List<Double> { 2.2, 3.3, 66.2, 17.5, 30.2, 31.1 };
 
-            Console.WriteLine("The sum of the array = {0}", Sum(testDataD, true));
+            //Changing the background color
+            Console.ForegroundColor = ConsoleColor.Green;
 
-            Console.WriteLine("The average of the array = {0}", Avg(testDataD, true));
+            //printing the sum of all the numbers in the TestDataD array
+            Console.WriteLine("The sum of the array = {0}", Sum(testDataD));
 
+            //printing the average of all the numbers in the TestDataD array
+            Console.WriteLine("The average of the array = {0}", Avg(testDataD));
+
+            //printing the Median of all the numbers in the TestDataD array
             Console.WriteLine("The median value of the Double data set = {0}", Median(testDataD));
 
+            //printing the Standard deviation of all the numbers in the TestDataD array
             Console.WriteLine("The sample standard deviation of the Double test set = {0}\n", SD(testDataD));
+
+            Console.ReadLine();
         }
     }
 }
